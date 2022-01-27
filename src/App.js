1d95ed9bebb97189e8bdeee11c313176eb2ea6ef
Rachel Solov <rsolov23@yahoo.com>
2022-01-26 18:09:59 -0600

    implemented preloader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { NavBar } from "./components/NavBar";
 import Footer from "./components/Footer";
-import ParticleBackground from "./pages/particle";
+import ParticleBackground from "./pages/Particle";
+import Preloader from "./pages/Pre";
+import "./App.css";
+
 
 function App() {
+  const [load, updateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <ChakraProvider>
-      <ParticleBackground />
-      <NavBar />
-      <Footer />
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <ParticleBackground />
+        <NavBar />
+        <Footer />
+      </div>
     </ChakraProvider>
   );
 }
