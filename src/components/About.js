7d12e14ff12a8faa4d279ee421883ce718368f9d
Rachel Solov <rsolov23@yahoo.com>
2022-01-30 21:00:31 -0600

    added tools and skills icons to about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,49 @@
-import React from 'react';
-
+import React from "react";
+import { Icon } from "@iconify/react";
+import { Box, Flex, Stack, chakra } from "@chakra-ui/react";
+import Skills from "../pages/Skills";
 export default function About() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Box p={5} m={10} borderRadius="md" id="tools">
+        <chakra.h1
+          display="flex"
+          justifyContent="center"
+          fontWeight="bold"
+          fontSize="2.5rem"
+          mb={5}
+        >
+          Tools I Use
+        </chakra.h1>
+        <Flex display="flex" justifyContent="space-evenly" mb={5}>
+          <Icon
+            className="test"
+            icon="logos:visual-studio-code"
+            width="75"
+            height="75"
+          />
+          <Icon
+            className="test"
+            icon="logos:heroku-icon"
+            width="75"
+            height="75"
+          />
+          <Icon
+            className="test"
+            icon="icon-park:github"
+            width="75"
+            height="75"
+          />
+          <Icon
+            className="test"
+            icon="logos:lighthouse"
+            width="75"
+            height="75"
+          />
+          <Icon className="test" icon="logos:git" width="75" height="75" />
+        </Flex>
+      </Box>
+      <Skills />
+    </>
+  );
 }
