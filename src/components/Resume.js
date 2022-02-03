543c536822c82ae1ffdc9c4e386e3f64b5131b42
Rachel Solov <rsolov23@yahoo.com>
2022-02-03 14:50:13 -0600

    added resume pdf to resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,17 +1,66 @@
-import React from "react";
-import { Flex, Box } from "@chakra-ui/react";
-import resume from "../assets/images/resume.png";
+import React, { useState } from "react";
+import { Box, Flex } from "@chakra-ui/react";
+import { Document, Page, pdfjs } from "react-pdf";
+import url from "../assets/images/resume.pdf";
 
-export default function Resume() {
+export default function Test() {
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  /*To Prevent right click on screen*/
+  // document.addEventListener("contextmenu", (event) => {
+  //   event.preventDefault();
+  // });
+
+  /*When document gets loaded successfully*/
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offset) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
+
   return (
     <Box>
       <Flex justifyContent="center">
-        <img
-          src={resume}
-          style={{ width: "50rem" }}
-          alt="resume"
-          className="resume"
-        />
+        <div className="main">
+          <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} />
+          </Document>
+          <div>
+            <div className="pagec">
+              Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+            </div>
+            <div className="buttonc">
+              <button
+                type="button"
+                disabled={pageNumber <= 1}
+                onClick={previousPage}
+                className="Pre"
+              >
+                Previous
+              </button>
+              <button
+                type="button"
+                disabled={pageNumber >= numPages}
+                onClick={nextPage}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
       </Flex>
     </Box>
   );
