5a2ad5f422f3d73306b07a0184d907c64647aaaa
Rachel Solov <rsolov23@yahoo.com>
2022-02-03 11:26:51 -0600

    added pdf renderer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Preloader from "./pages/Pre";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import About from "./components/About";
+import Resume from "./components/Resume";
 import "./App.css";
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
             <Route path="header" element={<Header />} />
             <Route path="about" element={<About />} />
             <Route path="projects" element={<Projects />} />
+            <Route path="resume" element={<Resume />} />
           </Routes>
           <Footer />
         </div>
