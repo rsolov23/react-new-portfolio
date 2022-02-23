dd7bd7decbc52ca8ec80d7ab4c10b00fa91615d3
Rachel Solov <rsolov23@yahoo.com>
2022-02-22 20:29:05 -0600

    deploying

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import ParticleBackground from "./pages/Particle";
+import ParticleBackground from "./pages/Background";
 import Preloader from "./pages/Pre";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
